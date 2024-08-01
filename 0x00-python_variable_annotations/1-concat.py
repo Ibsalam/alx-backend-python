5c74cf4478c513b591c3db85fec3418368106cc9
ibsalam <asio4edu@gmail.com>
2024-08-01 20:24:19 +0000

    cocat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Module containing the concat function.
+'''
+
+def concat(str1: string, str2: string) -> string:
+    '''Concatenates two strings.
+    '''
+    return str1 + str2
+
