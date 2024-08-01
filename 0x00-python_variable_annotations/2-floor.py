f4ea70e22f66667a37ede9e54bb56e10041fc9e8
ibsalam <asio4edu@gmail.com>
2024-08-01 20:39:02 +0000

    basic annotations floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Module containing the floor function
+'''
+
+
+def floor(a: float) -> int:
+    '''Returns the floor of a floating-point number.
+    '''
+    return int(a)
